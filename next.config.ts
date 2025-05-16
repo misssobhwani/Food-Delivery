f8b02c901c5cfc1d1c7fb6bb79d5eb5f9ecdfacd
Vijeta Sobhwani <vijetasobhwani@Vijetas-MacBook-Air.local>
2025-05-17 02:55:18 +0530

    6th commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ disables ESLint errors from breaking builds
+  },
 };
 
 export default nextConfig;
